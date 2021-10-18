ba0adffd90d8614dcbf7f5635ff3c0fcae06b596
sakhter <saqeb@dnnae.com>
2021-10-18 18:09:19 -0500

    initial daemon

--- EdgeAgent.py
+++ EdgeAgent.py
@@ -12,7 +12,7 @@ logging.basicConfig(filename=logFile,level=logging.DEBUG)
 class MyDaemon(daemon):
     def run(self):
         while True:
-            logging.debug('This message should go to the log file1')
+            logging.debug('This message should go to the log file')
             time.sleep(1)
 
 
