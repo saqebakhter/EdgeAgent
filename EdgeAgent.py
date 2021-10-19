840e6785e578b6a422e407626aa4a00bfe50df49
saqebakhter <saqeb.akhter@gmail.com>
2021-10-19 09:03:38 -0500

    update agent

--- EdgeAgent.py
+++ EdgeAgent.py
@@ -2,18 +2,40 @@ import os
 import logging
 import sys, time
 from daemon3x import daemon
-
+import subprocess
+from urllib import request, parse
+import json
 
 logFile = '/tmp/EdgeAgent.log'
-
+EdgeRegistrar = 'http://mockbin.org/bin/c890afbc-2e85-4c89-92ab-e606ce9e8f33'
 logging.basicConfig(filename=logFile,level=logging.DEBUG)
 
 
 class MyDaemon(daemon):
+    def getVMKIp(self):
+        logging.debug('get vmkip')
+        process = subprocess.Popen(['/bin/esxcli --debug --formatter=json network ip interface ipv4 get'],
+                                   shell=True,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        logging.debug(stdout)
+        parsedList = json.loads(stdout)
+        for interface in parsedList:
+            if interface['Name'] == 'vmk0':
+                return interface['IPv4Address']
+
     def run(self):
         while True:
             timestamp = int(time.time())
-            logging.debug('Edge Agent is running now ' + str(timestamp))
+            vmk0Ip = self.getVMKIp()
+            logging.debug('Edge Agent is running now ' + str(timestamp) + ' : ' + vmk0Ip)
+            payload = {'ipAddress': vmk0Ip}
+            encoded_data = json.dumps(payload).encode('utf-8')
+
+            req = request.Request(EdgeRegistrar, data=encoded_data)
+            resp = request.urlopen(req)
+
             time.sleep(30)
 
 
